1c86e113ed561650178835e94bfc4be9cbdf936a
Nafeesah-8 <kuforijinafisat8@gmail.com>
2022-03-17 13:36:00 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,17 @@
+#include <string.h>
 #include <stdio.h>
-
 #include <unistd.h>
-
-
-
 /**
- *
- *  *main - Entry point
- *
- *   *Return: Always 1
- *
- *    *
- *
- *     */
-
-
+ * main - doing well
+ * Return: 0 retu
+ */
 
 int main(void)
-
 {
+	int s;
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 	return (1);
-
 }
